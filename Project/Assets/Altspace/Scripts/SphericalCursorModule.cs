30fce0c538ed503d4d3db742758883950364b606
Peter J Galbraith <peter.j.galbraith@gmail.com>
2015-11-16 02:45:39 -0600

    Part 1 Complete
    Completed Part 1: 3D Cursor. Cursor now moves with mouse, travels to
    objects via raycast and then scales based on distance from player, and
    sits on an invisible sphere when not colliding. Cursor also has custom
    shader that draws on top of all other geometry and appears 2D

--- Project/Assets/Altspace/Scripts/SphericalCursorModule.cs
+++ Project/Assets/Altspace/Scripts/SphericalCursorModule.cs
@@ -24,30 +24,66 @@ public class SphericalCursorModule : MonoBehaviour {
 	private const float MaxDistance = 100.0f;
 
 	// Sphere radius to project cursor onto if no raycast hit.
-	private const float SphereRadius = 1000.0f;
+	private const float SphereRadius = 10.0f;
 
     void Awake() {
 		Cursor = transform.Find("Cursor").gameObject;
 		CursorMeshRenderer = Cursor.transform.GetComponentInChildren<MeshRenderer>();
-        CursorMeshRenderer.renderer.material.color = new Color(0.0f, 0.8f, 1.0f);
+        CursorMeshRenderer.GetComponent<Renderer>().material.color = new Color(0.0f, 0.8f, 1.0f);
     }	
 
 	void Update()
 	{
-		// TODO: Handle mouse movement to update cursor position.
+		//**********************************************************************************************************************
+		//Handle mouse movement to update cursor position.
+		//**********************************************************************************************************************
 
-		// TODO: Perform ray cast to find object cursor is pointing at.
-		// TODO: Update cursor transform.
-		var cursorHit = new RaycastHit();/* Your cursor hit code should set this properly. */;
+		//Read in input from the mouse (and scale them by the defined sensitivity)
+		float mouseX = Input.GetAxis ("Mouse X") * Sensitivity;
+		float mouseY = Input.GetAxis ("Mouse Y") * Sensitivity;
+
+		//Figure out the distance from the camera object to the cursor
+		float camToCursorDistance = Vector3.Distance (transform.position, Cursor.transform.position);
+
+		//Move the cursor by scaling X and Y movements and applying those movements to the cursor's transform
+		Vector3 amountToMoveCursor = new Vector3 (mouseX * camToCursorDistance, mouseY * camToCursorDistance);
+		Cursor.transform.Translate (amountToMoveCursor);
+
+		//Find the new direction to the cursor (as a unit vector) and ensure cursor is facing that direction so it behaves as though it is on a sphere
+		Vector3 camToCursorDirection = (Cursor.transform.position - transform.position).normalized;
+		Cursor.transform.forward = camToCursorDirection;
+
+		//**********************************************************************************************************************
+		//Perform ray cast to find object cursor is pointing at.
+		//Update cursor transform.
+		//**********************************************************************************************************************
+		var cursorHit = new RaycastHit();
+
+		//Perform raycast from the camera position in the direction of the cursor, but limit the interaction distance and interactable objects to our variables
+		Physics.Raycast (transform.position, camToCursorDirection, out cursorHit, MaxDistance, ColliderMask);
 
 		// Update highlighted object based upon the raycast.
 		if (cursorHit.collider != null)
 		{
 			Selectable.CurrentSelection = cursorHit.collider.gameObject;
+
+			//Set the cursor's position to be the point on the surface of the collider where the raycast hit
+			Cursor.transform.position = cursorHit.point;
+
+			//Calculate the cursor's scale (based on the formula given in the instructions) and apply it to the cursor (after flattening the cursor based on the surface)
+			float calulatedCusorScale = (cursorHit.distance * DistanceScaleFactor + 1.0f) / 2.0f;
+			Cursor.transform.localScale = Vector3.one * calulatedCusorScale;
 		}
 		else
 		{
 			Selectable.CurrentSelection = null;
+
+			//Set the cursor's position to the local origin and then move it forward/out
+			Cursor.transform.localPosition = Vector3.zero;
+			Cursor.transform.Translate(Vector3.forward * SphereRadius);
+
+			//Set the cursor's scale to be the default scale defined in our application
+			Cursor.transform.localScale = DefaultCursorScale;
 		}
 	}
 }
---
+++ Project/Assets/_Shaders/Cursor.shader
@@ -0,0 +1,55 @@
+﻿Shader "Custom/Cursor"
+{
+	Properties
+	{
+		_Color ("Color", Color) = (1,1,1,1)
+	}
+	SubShader
+	{
+		Tags { "Queue" = "Overlay" "RenderType" = "Opaque" }
+		Pass
+		{
+			ZTest Always
+			Lighting Off
+			CGPROGRAM
+
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            float4 vert(float4 v:POSITION) : SV_POSITION {
+                return mul (UNITY_MATRIX_MVP, v);
+            }
+            
+            fixed4 _Color;
+
+            fixed4 frag() : SV_Target {
+            	fixed4 c = fixed4 (1,1,1,1);
+                return c;
+            }
+
+            ENDCG
+		}
+		Pass
+		{
+			ZTest Always
+			Lighting Off
+			CGPROGRAM
+
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            float4 vert(float4 v:POSITION) : SV_POSITION {
+                return mul (UNITY_MATRIX_MVP, v);
+            }
+            
+            fixed4 _Color;
+
+            fixed4 frag() : SV_Target {
+            	fixed4 c = _Color;
+                return c;
+            }
+
+            ENDCG
+		}
+	}
+}
